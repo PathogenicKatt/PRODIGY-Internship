6b19e378ee8dcfd23f30289902a27270b993c97e
Katleho Mofokeng <katlehom814@gmail.com>
2025-07-18 17:08:49 +0200

    packet-sniffer

--- PRODIGY_CS_05/packet_analyzer.py
+++ PRODIGY_CS_05/packet_analyzer.py
@@ -10,7 +10,6 @@ DELAY_SECONDS = 2    # Time between outputs
 def log_visit(packet):
     if not packet.haslayer(TCP) or not packet.haslayer(IP):
         return
-    
     src = f"{packet[IP].src}:{packet[TCP].sport}"
     dst = f"{packet[IP].dst}:{packet[TCP].dport}"
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -23,34 +22,27 @@ def log_visit(packet):
             return log + "\n"
         except:
             pass
-    
-    
     elif packet[TCP].dport == 443: # HTTPS (Port 443)
         log = f"[{timestamp}] HTTPS: {src} -> {packet[IP].dst} (Port 443)"
         print(log)
         return log + "\n"
-    
     return None
 
 def main():
     print(f"\n...Monitoring web traffic (HTTP/HTTPS)...\n")
     print("Press Ctrl+C to stop\n")
-    
     packet_count = 0
     with open(LOG_FILE, "a") as log_file:
         def callback(packet):
             nonlocal packet_count
             if packet_count >= PACKET_LIMIT:
-                return
-            
+                return  
             log_entry = log_visit(packet)
             if log_entry:
                 log_file.write(log_entry)
                 packet_count += 1
                 time.sleep(DELAY_SECONDS)  
-        
         sniff(prn=callback, store=False, filter="tcp port 80 or tcp port 443")
-
     print(f"\n...Captured {packet_count} packets. Log saved to '{LOG_FILE}'...")
 
 if __name__ == "__main__":
